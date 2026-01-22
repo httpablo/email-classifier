4de5c5f3738d6971fa681814a8ee93f6db03d0f7
httpablo <pablobrith@gmail.com>
2026-01-22 00:46:10 -0300

    url da produção

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://email-classifier-pablo.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
